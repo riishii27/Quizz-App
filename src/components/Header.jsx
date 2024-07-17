6deac42b19a3148b307161b6cb9d2b2ad84f03ac
Rishi <rishisrivastava2706@gmail.com>
2024-07-17 15:10:12 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import quizImg from '../assets/quiz-logo.png'
+
+export default function Header(){
+    return(
+        <header>
+            <img src={quizImg} alt='quiz logo' />
+            <h1>ReactQuiz</h1>
+        </header>
+    );
+}
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,31 @@
+import {useState, useCallback } from "react";
+import QUESTIONS from '../questions'
+import Question from "./Question";
+import Summary from "./SUmmary";
+
+export default function Quiz(){
+ const [userAnswers, setUserAnswers]= useState([]);
+ 
+ const activeQuestionIndex = userAnswers.length;
+ const quizIsComplete = activeQuestionIndex === QUESTIONS.length
+
+ const handleSelectAnswer = useCallback( function handleSelectAnswer(selectedAnswer){
+    
+    setUserAnswers((prevUserAnswers)=>{
+        return [...prevUserAnswers, selectedAnswer];
+    });},
+    []);
+
+ const handleSkipAnswer = useCallback(()=>handleSelectAnswer(null),[handleSelectAnswer])
+
+    if(quizIsComplete){
+        return(
+            <Summary userAnswers={userAnswers}/>
+        );
+    }
+    return(
+        <div id="quiz">
+            <Question index={activeQuestionIndex} key={activeQuestionIndex} onSkipAnswer={handleSkipAnswer}   onSelectAnswer={handleSelectAnswer}/>
+        </div>
+    )
+}
